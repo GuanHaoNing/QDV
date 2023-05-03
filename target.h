54030d91874e32df2a764dacb2198c6d1d929543
GuanHaoNing <129737617+GuanHaoNing@users.noreply.github.com>
2023-05-03 13:22:48 +0800

    Create target.h

---
+++ target.h
@@ -0,0 +1,9 @@
+//little_target.h
+#ifndef LITTLE_TARGET
+#define LITTLE_TARGET
+#include "structure_define.h"
+
+//在这里declare其他函数
+void business(int &cash, bool* isBankrupt, bool* isSetUpCompany, company* company_list, string* company_charlist);
+
+#endif
